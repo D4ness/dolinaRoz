e6e771cf027a94dd82f634bf5868d66ed6ec83b5
D4ness <lyakhov016@gmail.com>
2021-08-18 11:05:15 +0300

    bg-video for prod

--- scripts/production.js
+++ scripts/production.js
@@ -22,3 +22,26 @@ const contacts = document.querySelector('#Контакты');
 nav_contact.addEventListener('click', () => {
     scrollTo(contacts);
 })
+const nav_prices = document.getElementById('nav_prices');
+const prices = document.querySelector('#Цены');
+nav_prices.addEventListener('click', () => {
+    scrollTo(prices);
+})
+
+
+
+let winWidth1 = window.innerWidth;
+let bgVideo1 = 1920;
+let bgBlock1 = document.getElementById('bg_block');
+if (bgVideo1 >= winWidth1){
+    bgBlock1.style.left = (-(bgVideo1-winWidth1)/2)+"px";
+}
+window.addEventListener(`resize`, event => {
+    let winWidth = window.innerWidth;
+    let bgVideo = 1920;
+    let bgBlock = document.getElementById('bg_block');
+    if (bgVideo >= winWidth){
+        bgBlock.style.left = (-(bgVideo-winWidth)/2)+"px";
+    }
+    console.log(bgVideo);
+}, false);
